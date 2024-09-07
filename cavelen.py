8d5f770e5a73c5d8aecc894163a5fc26176bb334
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-09-07 18:14:58 +0200

    Add a desc to cavelen.py script

--- cavelen.py
+++ cavelen.py
@@ -1,6 +1,12 @@
 import pefile
 import sys
 
+'''
+Utility script to check the code cave length of the specified PE.
+Usage:
+    python cavelen.py PE_FILE
+'''
+
 
 def find_code_cave_len(pe: pefile.PE):
     raw_end = pe.sections[0].PointerToRawData + \
@@ -14,4 +20,3 @@ def find_code_cave_len(pe: pefile.PE):
 pe = pefile.PE(sys.argv[1], fast_load=False)
 cave_len = find_code_cave_len(pe)
 print(f'Code cave len: {cave_len}')
-
